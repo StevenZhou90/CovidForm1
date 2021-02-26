11048a5aeb54301abb5b341cb01cd52ff1ab6ef9
StevenZhou90 <70081222+StevenZhou90@users.noreply.github.com>
2021-02-26 13:03:14 -0700

    Update intro.js

--- components/intro.js
+++ components/intro.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { Formik } from 'formik';
-  
+import * as yup from 'yup';
+
+const schema = yup.object({
+    name: yup.string().required('This is a required field'),
+    dateOfBirth: yup.string().required('This is a required field'),
+    primaryLanguage: yup.string().required('This is a required field'),
+    useOfInterpreeter: yup.string().required('This is a required field'),
+})
+
 export default function Intro({navigation}){
     return(
-        <View>
+        <View style={styles.screen}> 
             <Formik
-            initialValues={{name: '', dateOfBirth: '', primaryLanguage: '', useOfInterpreeter: ''}}
-            onSumbit={(values) => {
-                console.log(values);
-            }}
+                initialValues={{name: '', dateOfBirth: '', primaryLanguage: '', useOfInterpreeter: ''}}
+                validationSchema={schema}
+                onSubmit={(values, schema) => {
+                    console.log(values);
+                    navigation.navigate('Demo');
+                }}
             >
-            {(props) => (
+            {(props, isValid, errors) => (
                 <View
                 >
                     <View>
@@ -21,7 +31,9 @@ export default function Intro({navigation}){
                             placeholder=''
                             onChangeText={props.handleChange('name')}
                             value={props.values.name}
+                            onBlur={props.handleBlur('name')}
                         />  
+                        <Text style={styles.error}>{props.touched.name && props.errors.name}</Text>
                     </View>   
 
                     <View >
@@ -32,7 +44,9 @@ export default function Intro({navigation}){
                             placeholder='MM-DD-YY'
                             onChangeText={props.handleChange('dateOfBirth')}
                             value={props.values.dateOfBirth}
+                            onBlur={props.handleBlur('dateOfBirth')}
                         />  
+                        <Text style={styles.error}>{props.touched.dateOfBirth && props.errors.dateOfBirth}</Text>
                     </View>
                 
                     <View>
@@ -42,7 +56,9 @@ export default function Intro({navigation}){
                             placeholder=''
                             onChangeText={props.handleChange('primaryLanguage')}
                             value={props.values.primaryLanguage}
+                            onBlur={props.handleBlur('primaryLanguage')}
                         />  
+                        <Text style={styles.error}>{props.touched.primaryLanguage && props.errors.primaryLanguage}</Text>
                     </View>
 
                     <View>
@@ -52,12 +68,15 @@ export default function Intro({navigation}){
                             placeholder=''
                             onChangeText={props.handleChange('useOfInterpreeter')}
                             value={props.values.useOfInterpreeter}
+                            onBlur={props.handleBlur('useOfInterpreeter')}
                         />  
+                        <Text style={styles.error}>{props.touched.useOfInterpreeter && props.errors.useOfInterpreeter}</Text>
                     </View>
                       
                     <Button 
-                        title='Next' onPress={props.handleSumbit}
-                        onPress={() => navigation.navigate('Demo')}
+                        style={styles.button}
+                        title='Next' 
+                        onPress={() => {props.handleSubmit()}}
                     />
                 </View>
             )}
@@ -68,16 +87,35 @@ export default function Intro({navigation}){
 
 const styles = StyleSheet.create({
     question: {
-        fontSize:18,
+        fontSize:24,
         paddingLeft: 10,
         paddingTop:10,
+        color: '#012A4A',
+        
         },
     input: {
-        borderWidth: 2,
-        borderColor: "#ddd",
-        fontSize: 18,
-        borderRadius: 6,
+        borderWidth: 3,
+        borderColor: "#2C7DA0",
+        fontSize: 24,
+        borderRadius: 10,
         paddingLeft: 10,
         margin: 10,
+        backgroundColor: '#A9D6E5', 
         },
+    screen: {
+        fontSize:24,
+        paddingLeft: 10,
+        paddingTop:10,
+        backgroundColor: '#A9D6E5',
+        flex: 1,
+        },
+    button: {
+        backgroundColor: '#03045e',
+    },
+    error: {
+        textAlign: 'center',
+        fontSize: 18,
+        color: '#e7305b'
+    }
+    
     });
